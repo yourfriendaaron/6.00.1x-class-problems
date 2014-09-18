1d680ad31b0364016dd19ca4dca86c13995912f8
yourfriendaaron <yourfriendaaron@users.noreply.github.com>
2014-09-17 21:43:38 -0400

    Create L4 PROBLEM 10.py

---
+++ L4 PROBLEM 10.py
@@ -0,0 +1,12 @@
+def isVowel(char):
+    '''
+    char: a single letter of any case
+
+    returns: True if char is a vowel and False otherwise.
+    '''
+    # Your code here
+    if char == 'a' or char == 'e' or char == 'i' or char == 'o'or char == 'u'\
+        or char == 'A' or char == 'E' or char == 'I' or char == 'O' or char == 'U':
+        return True
+    else:
+        return False
