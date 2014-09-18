9d50dbe9862a1a61e14b3cc7956e8a16cb12d7c2
yourfriendaaron <yourfriendaaron@users.noreply.github.com>
2014-09-17 21:27:47 -0400

    Create L4 PROBLEM 9.py

---
+++ L4 PROBLEM 9.py
@@ -0,0 +1,13 @@
+def square(x):
+    '''
+    x: int or float.
+    '''
+    result = x*x
+    return result
+
+def fourthPower(x):
+    '''
+    x: int or float.
+    '''
+    result = (square(x))*(square(x))
+    return result
